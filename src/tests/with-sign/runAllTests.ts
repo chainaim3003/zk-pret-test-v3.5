ab52dd7d36cb73da60ec46a487baf2d1f7101d62
DeepanB2005 <deepanb2005@gmail.com>
2025-06-16 00:37:37 +0530

    runAllTests.ts updation

--- src/tests/with-sign/runAllTests.ts
+++ src/tests/with-sign/runAllTests.ts
@@ -74,6 +74,40 @@ function runAllTests() {
       console.log('----------------------------------BusinessStandardDataIntegrity=> \src\data\scf\BILLOFLADING\BOL-INVALID-2.json----------------------------------------');
       execSync('node ./build/tests/with-sign/BusinessStandardDataIntegrityVerificationTest.js .\src\data\scf\BILLOFLADING\BOL-INVALID-2.json', { stdio: 'inherit' });
 
+      console.log('------------------------------------------------------------------------------------------------------------------------');
+      console.log('---------------------------------/build/tests/with-sign/RiskLiquidityBasel3OptimMerkleVerificationTestWithSign.js 100 100 http://98.84.165.146:8083/eventsBatch src/data/RISK/Basel3/CONFIG/basel3-VALID-2.json----------------------------------------');
+      execSync('node ./build/tests/with-sign/RiskLiquidityBasel3OptimMerkleVerificationTestWithSign.js 100 100 http://98.84.165.146:8083/eventsBatch src/data/RISK/Basel3/CONFIG/basel3-VALID-2.json', { stdio: 'inherit' });
+
+      console.log('------------------------------------------------------------------------------------------------------------------------');
+      console.log('----------------------------------/build/tests/with-sign/CorporateRegistrationOptimSingleCompanyVerificationTestWithSign.js----------------------------------------');
+      execSync('node ./build/tests/with-sign/CorporateRegistrationOptimSingleCompanyVerificationTestWithSign.js "U01112TZ2022PTC039493"', { stdio: 'inherit' });
+      
+      console.log('------------------------------------------------------------------------------------------------------------------------');
+      console.log('----------------------------------/build/tests/with-sign/CorporateRegistrationOptimMultiCompanyVerificationTestWithSign.js----------------------------------------');
+      execSync('node ./build/tests/with-sign/CorporateRegistrationOptimMultiCompanyVerificationTestWithSign.js "U01112TZ2022PTC039493"', { stdio: 'inherit' });
+
+      console.log('------------------------------------------------------------------------------------------------------------------------');
+      console.log('----------------------------------/build/tests/with-sign/EXIMOptimSingleCompanyVerificationTestWithSign.js----------------------------------------');
+      execSync('node ./build/tests/with-sign/EXIMOptimSingleCompanyVerificationTestWithSign.js "SREE PALANI ANDAVAR AGROS PRIVATE LIMITED"', { stdio: 'inherit' });
+
+      console.log('------------------------------------------------------------------------------------------------------------------------');
+      console.log('----------------------------------/build/tests/with-sign/EXIMOptimMultiCompanyVerificationTestWithSign.js----------------------------------------');
+      execSync('node ./build/tests/with-sign/EXIMOptimMultiCompanyVerificationTestWithSign.js "SREE PALANI ANDAVAR AGROS PRIVATE LIMITED"', { stdio: 'inherit' });
+      
+      console.log('------------------------------------------------------------------------------------------------------------------------');
+      console.log('----------------------------------/build/tests/with-sign/GLEIFOptimSingleCompanyVerificationTestWithSign.js----------------------------------------');
+      execSync('node ./build/tests/with-sign/GLEIFOptimSingleCompanyVerificationTestWithSign.js "SREE PALANI ANDAVAR AGROS PRIVATE LIMITED"', { stdio: 'inherit' });
+      
+      console.log('------------------------------------------------------------------------------------------------------------------------');
+      console.log('----------------------------------/build/tests/with-sign/GLEIFOptimMultiCompanyVerificationTestWithSign.js----------------------------------------');
+      execSync('node ./build/tests/with-sign/GLEIFOptimMultiCompanyVerificationTestWithSign.js "SREE PALANI ANDAVAR AGROS PRIVATE LIMITED"', { stdio: 'inherit' });
+      
+      console.log('------------------------------------------------------------------------------------------------------------------------');
+      console.log('----------------------------------/build/tests/with-sign/ComposedRecursiveOptim3LevelVerificationTestWithSign.js----------------------------------------');
+      execSync('node ./build/tests/with-sign/ComposedRecursiveOptim3LevelVerificationTestWithSign.js "SREE PALANI ANDAVAR AGROS PRIVATE LIMITED" "U01112TZ2022PTC039493"', { stdio: 'inherit' });
+      
+
+
       console.log('All tests finished successfully.');
    } catch (error) {
       console.error('Error running one of the scripts:', error);
